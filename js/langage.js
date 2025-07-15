5de71ca2b301109b366ffd3f17e42636ceace5ef
Noé Samulunda <46996718+noesamulunda@users.noreply.github.com>
2025-07-15 12:25:55 +0100

    Add files via upload

---
+++ js/langage.js
@@ -0,0 +1,168 @@
+const translations = {
+      pt: {
+        fala: "Fale, comigo!",
+        home: "INICIO",
+        sobre: "SOBRE",
+        solucao: "SOLUÇÕES",
+        programador: "PROGRAMADOR",
+        contato: "CONTACTO",
+        footer_home: "Inicio",
+        footer_sobre: "Sobre nós",
+        footer_solucoes: "Soluções",
+        footer_programador: "Programador",
+        footer_contato: "Contacto",
+        footer_servico: "Serviços",
+        footer_parceiro: "Parceiro",
+        footer_termos: "Termos de uso",
+        footer_politica: "Política e privacidade",
+        api: "INTEGRAR ",
+        titulo1: "Soluções de Pagamentos em <br> Tempo Real!",
+        subtitulo1: "Com as soluções de pagamentos da Intelize, é possível<br> efectuar transações financeiras com qualquer pessoa, <br> em qualquer lugar e a qualquer hora.",
+        btn_integrar: "INTEGRAR AGORA",
+        titulo2: "API Inovadora para Potencializar <br> Seu Sistema de Pagamento",
+        subtitulo2: "Uma solução inovadora projetada para aprimorar e impulsionar o sistema <br> de pagamento dos usuários proporcionando uma experiência mais eficiente e eficaz.",
+        titulo3: "Suporte para diversos  Meios <br> de Pagamento!",
+        subtitulo3: "Sistema Integrado com suporte para operações via ATM,<br>TPA, Multicaixa Express e Internet Banking.",
+        sobrenos: "Sobre Nós",
+        sobretexto1: "Somos uma Empresa Certificada pela EMIS como Facilitador Tecnológico de Pagamentos nas Classes PST - SP Classe 1 e 2, passamos pelas Verificações de Segurança e Testes de Utilização realizadas pela EMIS.",
+        sobretexto2: "Temos a oferecer uma Plataforma que facilita a Integração ao Sistema de Pagamentos Angolano, a fim de facilitar e optimizar os processos de Pagamentos.",
+        nossasolucao: "Nossas soluções",
+        nossoparceiro: "Nossos parceiros",
+        visitante: "Visitantes",
+        vermais:"Ver mais",
+        text_desc: "Construimos a melhor Experiência para os nossos Clientes!",
+        text_get: "Gateway de Pagamentos",
+        text_get_text: "A solução ideal para empresas que buscam receber pagamentos de forma eficiente em seus sites ou lojas virtuais. Com a nossa plataforma, você pode aceitar pagamentos de produtos e serviços sem a necessidade de leitura de cartão.",
+        text_pr: "Pagamentos por Referência",
+        text_pr_text: "Proporcione aos seus clientes uma variedade de opções de pagamento, a inclusão do pagamento por referência no seu negocio é uma escolha estratégica. Facilite a vida dos seus clientes oferecendo mais uma forma de realizar pagamentos.",
+        text_po: "Pagamentos Online (GPO)",
+        text_po_text: "Oferecemos aos comerciantes uma abordagem inovadora para receber pagamentos de compras e serviços em suas lojas virtuais. Com a nossa plataforma, sua experiência de aceitar transações online atinge um novo patamar de praticidade.",
+        integracao2: "Integração",
+        integracao_hoje: "Integre Connosco Hoje!",
+        integracao_text: "Soluções para micro, pequenas e grandes empresas, tenha controle de todos os pagamentos recebidos sem enfretar filas.",
+        integracao_btn: "INTEGRAR AGORA",
+        contact_nova: "Entre em contacto!",
+        contact_onde: "Estamos ansiosos para ouvir de você.",
+        enviar: "Enviar mensagem",
+        empresa: "Empresa",
+        telefone: "Telefone",
+        email: "Endereço de email",
+        assunto: "Assunto",
+        mensagem: "Mensagem",
+        seguir: "Siga-nos",
+        link1: "Nossos links",
+        link2: "Outros links",
+        link3: "Contate-nos",
+        footer_telefone: "Telefone:",
+        footer_email: "E-mail:",
+        foote_copy: "Todos os direitos reservados.",
+        nossasolucao1: "Comentários",
+        text_desc1:"O que os Clientes falam!",
+         noticia:"Últimas Notícias",
+         endereco:"Endereço",
+         ligue:"Ligue para nós",
+         enviaremail:"Envie-nos um e-mail"
+
+      },
+      en: {
+        fala: "Talk to me!",
+        home: "START",
+        sobre: "ABOUT",
+        solucao: "SOLUTIONS",
+        programador: "PROGRAMMER",
+        contato: "CONTACT",
+        footer_home: "Start",
+        footer_sobre: "About us",
+        footer_solucoes: "Solutions",
+        footer_programador: "Programmer",
+        footer_contato: "Contact",
+        footer_servico: "Services",
+        footer_parceiro: "Partner",
+        footer_termos: "Terms of use",
+        footer_politica: "Policy and privacy",
+        api: "INTEGRATE",
+        vermais:"See more",
+        titulo1: "Real-Time Payment Solutions!",
+        subtitulo1: "With Intelize's payment solutions, it is possible <br> to carry out financial transactions with anyone, anywhere and at any time.",
+        btn_integrar: "INTEGRATE NOW",
+        titulo2: "Innovative API to Enhance <br> Your Payment System",
+        subtitulo2: "An innovative solution designed to enhance and boost the system <br> user payment methods providing a more efficient and effective experience.",
+        titulo3: "Support for various payment methods!",
+        subtitulo3: "Integrated System with support for operations via ATM,<br>TPA, Multicaixa Express and Internet Banking.",
+        sobrenos: "About Us",
+        sobretexto1: "We are a Company Certified by EMIS as a Technological Payment Facilitator in the PST - SP Classes 1 and 2, we have passed the Security Checks and Usage Tests carried out by EMIS.",
+        sobretexto2: "We offer a Platform that facilitates Integration into the Angolan Payment System, in order to facilitate and optimize Payment processes.",
+        nossasolucao: "Our solutions",
+         nossoparceiro: "Our partners",
+        visitante: "Visitors",
+        text_desc: "We build the best Experience for our Customers!",
+        text_get: "Payment Gateway",
+        text_get_text: "The ideal solution for companies looking to efficiently accept payments on their websites or online stores. With our platform, you can accept payments for products and services without the need for card reading.",
+        text_pr: "Reference Payments",
+        text_pr_text: "Provide your customers with a variety of payment options. Including payment by reference in your business is a strategic choice. Make your customers' lives easier by offering them another way to make payments.",
+        text_po: "Online Payments (GPO)",
+        text_po_text: "We offer merchants an innovative approach to accepting payments for purchases and services in their online stores. With our platform, your experience of accepting online transactions reaches a new level of convenience.",
+        integracao2: "Integration",
+        integracao_hoje: "Join Us Today!",
+        integracao_text: "Solutions for micro, small and large companies, have control over all payments received without having to wait in lines.",
+        integracao_btn: "INTEGRATE NOW",
+        contact_nova: "Get in touch!",
+        contact_onde: "We look forward to hearing from you.",
+        enviar: "Send message",
+        empresa: "enterprise",
+        telefone: "telephone",
+        email: "email address",
+        assunto: "Subject",
+        mensagem: "Message",
+        seguir: "Follow us",
+        link1: "Our links",
+        link2: "Other links",
+        link3: "Contact us",
+        footer_telefone: "Telephone:",
+        footer_email: "E-mail:",
+        foote_copy: "All rights reserved.",
+        nossasolucao1: "Feadbacks",
+        text_desc1:"What Customers Say!",
+        noticia:"Latest News",
+        endereco:"Address",
+         ligue:"Call us",
+         enviaremail:"Send us an email"
+
+
+      }
+    };
+
+    function translate(lang) {
+    $('[data-translate]').each(function () {
+        const key = $(this).data('translate');
+        const isHtml = $(this).data('html') === true || $(this).data('html') === "true";
+        if (translations[lang][key]) {
+        if (isHtml) {
+            $(this).html(translations[lang][key]); // Preserva <br>, <b>, etc.
+        } else {
+            $(this).text(translations[lang][key]); // Simples texto
+        }
+        }
+    });
+
+    $('[data-placeholder]').each(function () {
+        const key = $(this).data('placeholder');
+        if (translations[lang][key]) {
+        $(this).attr('placeholder', translations[lang][key]);
+        }
+    });
+    }
+
+
+    // Inicialmente português
+    $(document).ready(function () {
+      translate('pt');
+
+      $('#btn-pt').click(function () {
+        translate('pt');
+      });
+
+      $('#btn-en').click(function () {
+        translate('en');
+      });
+    });
